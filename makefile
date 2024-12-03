da5cde76f149126589e6b87ece4fa6a49a3c9d64
linabigail <alin50@stuy.edu>
2024-12-02 22:15:43 -0500

    finished basic version of prompt, updated makefile to do commands silently.

--- makefile
+++ makefile
@@ -1,4 +1,6 @@
 compile: shell.o
-	gcc -o shell shell.o
+	@gcc -o shell shell.o
 shell.o: shell.c
-	gcc -c shell.c
+	@gcc -c shell.c
+clean: 
+	@rm -f *.o shell
--- shell.c
+++ shell.c
@@ -9,27 +9,38 @@ int err(){
   exit(1);
 }
 
-void read_csv(){
-  FILE *data = fopen("test.txt","rb");
-  if (data == NULL){
-    printf("broken\n");
-    err();
-    return;
+void parse_args( char * line, char ** arg_ary ){
+  char * temp = line;
+  int c = 0;
+  while((arg_ary[c] = strsep(&temp, " "))){
+    printf("%s\n", arg_ary[c]);
+    c++;
   }
+  arg_ary[c + 1] = NULL;
 }
 
 void prompt(){
-  FILE *data = fopen("test.txt","rb");
-  if (data == NULL){
-    printf("broken\n");
-    err();
-    return;
+  // FILE *data = fopen("test.txt","rb");
+  // if (data == NULL){
+  //   printf("broken\n");
+  //   err();
+  //   return;
+  // }
+  char input[1000]; // adjust
+  printf("$ "); // mimic terminal
+  fflush(stdout);
+  if (fgets(input, sizeof(input), stdin) == NULL){
+    perror("could not get input\n");
+    exit(1);
   }
-  char input[100]; // adjust
-  scanf("%s",input);
+  // scanf("%s",input);
   printf("%s",input);
 }
 
 int main(){
-  prompt();
+  char input[1000];
+  while (1){
+    prompt();
+  }
+  return 0;
 }
