c362a1e788022a8c0628a49aaab47489246d5b2a
Abigail Lin <linabigail327@gmail.com>
2024-12-03 15:19:29 -0500

    working on cntrl d

--- shell.c
+++ shell.c
@@ -23,16 +23,18 @@ void parse_args( char * line, char ** arg_ary ){
 void prompt(){
 	char* temp;
   chdir(getenv("HOME"));
-  char cwd[1000];
+  char cwd[1024];
   if (getcwd(cwd, sizeof(cwd)) == NULL){
     perror("could not get path\n");
     exit(1);
   }
-  char input[1000]; // adjust
-  printf("%s $ ",cwd); // mimic terminal
-  fflush(stdout);
+  char input[1024]; // adjust
 	temp = fgets(input, sizeof(input), stdin);
   if (temp == NULL){
+    if (feof(stdin)){
+      printf("exit (end of file)\n");
+      exit(1);
+    }
     perror("could not get input");
     exit(1);
   }
@@ -42,6 +44,8 @@ void prompt(){
 	else {
 		printf("%s",input);
 	}
+  printf("%s $ ",cwd); // mimic terminal
+  fflush(stdout);
 }
 
 int main(){
